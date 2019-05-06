8e9efac1a21087a87f1b990e849b324d9061db55
willyb321 <wbwilliam7@gmail.com>
2019-05-07 07:19:05 +1000

    update deps

--- generators/app/templates/src/app.ts
+++ generators/app/templates/src/app.ts
@@ -25,26 +25,13 @@ const cacheTime: number = 86400000 * 7;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(favicon(join(__dirname, '..', 'public', 'favicon.ico')));
-
 app.use(responseTime());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 
-// view engine setup
-app.set('views', join(__dirname, '..', 'views'));
-app.set('view engine', 'pug');
 
-app.use(sassMiddleware({
-	src: join(__dirname, '..', 'public'),
-	dest: join(__dirname, '..', 'public'),
-	indentedSyntax: true, // true = .sass and false = .scss
-	sourceMap: true
-}));
-
-app.use(express.static(join(__dirname, '..', 'public'), {maxAge: cacheTime}));
 app.use('/', index);
 
 export default app;
--- generators/app/templates/src/routes/index.ts
+++ generators/app/templates/src/routes/index.ts
@@ -3,7 +3,7 @@ import * as express from 'express';
 const router: express.Router = express.Router();
 
 router.get('/', (req: express.Request, res: express.Response) => {
-	res.render('index', {title: 'Hello, World!'});
+	return res.send('Hello from generator-willyb-web')
 });
 
 export default router;
